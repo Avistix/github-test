96a6c4bd4109b5a2af47993686243df42681f298
Avistix <avistixs@gmail.com>
2020-04-10 21:35:44 +0530

    createdesign1

---
+++ github-test/t-form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace github_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnview = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnview
+            // 
+            this.btnview.Location = new System.Drawing.Point(103, 104);
+            this.btnview.Name = "btnview";
+            this.btnview.Size = new System.Drawing.Size(75, 23);
+            this.btnview.TabIndex = 0;
+            this.btnview.Text = "view";
+            this.btnview.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Click to View Next Page";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnview);
+            this.Name = "Form1";
+            this.Text = "t-form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnview;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ github-test/t-form2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace github_test
+{
+    public partial class t_form2 : Form
+    {
+        public t_form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
