275008554e03a4d69ed374e45067e935473ad34c
Avistix <avistixs@gmail.com>
2020-04-11 23:47:20 +0530

    change-2

--- github-test/t-form2.Designer.cs
+++ github-test/t-form2.Designer.cs
@@ -44,7 +44,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(58, 133);
+            this.label2.Location = new System.Drawing.Point(56, 134);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(165, 13);
             this.label2.TabIndex = 1;
